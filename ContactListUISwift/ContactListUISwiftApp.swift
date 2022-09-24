8928667fc656d86764a81a867a8e766b39e63046
Bellaaspera <lanasenatorova@gmail.com>
2022-09-24 12:46:45 +0300

    Initial Commit

---
+++ ContactListUISwift/ContactListUISwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListUISwiftApp.swift
+//  ContactListUISwift
+//
+//  Created by Светлана Сенаторова on 24.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListUISwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
